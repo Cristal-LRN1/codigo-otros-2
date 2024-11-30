ab9a5612b643c011563cde1039b8d55442578ed4
Cristal-LRN1 <cristal.loreto.rn@gmail.com>
2024-11-29 22:16:48 -0600

    Este es mi código final

--- formulario.js
+++ formulario.js
@@ -1,34 +1,42 @@
-var formulario = document.querySelector(".formulario") // No existe un id #form, se cambia a .formulario
+//Se cambian los var por let para evitar errores en el código
+let formulario = document.querySelector(".formulario") // No existe un id #form, se cambia a .formulario
 
 formulario.onsubmit = function(e) {
+  console.log('onSubmit')
 
-  e.preventDeFault();// Se corrige la escritura de preventDefault()
+  e.preventDefault();// Se corrige la escritura de preventDefault()
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  let nb = formulario.elements[0] // Se cambia el nombre de la variable para evitar errores
+  let edd = formulario.elements[1] // Se cambia el nombre de la variable e para evitar conflictos con la función e.preventDefault()
+  let na = formulario.elements[2]
 
-  var nombre = n.value
-  var edad = e.value
+  let nombre = nb.value
+  let edad = edd.value
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
+  let i = na.selectedIndex
+  let nacionalidad = na.options[i].value
   console.log(nombre, edad)
   console.log(nacionalidad)
 
+
   if (nombre.length === 0) {
-    n.classList.add("error")
+    nom.classList.add("error")
   }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+  else if (edad < 18 || edad > 120) { //Corregir a else if
+    ed.classList.add("error")
   }
-
-if (nombre.length > 0 && (edad> 18 || edad<120)) {
+  else if (nombre.length > 0 && (edad > 18 && edad < 120) ) { //Corregir a else if
   agregarInvitado(nombre, edad, nacionalidad)
+  console.log('en orden');
   }
-};
+}
 
-//Se elimina boton de borrado por estar dupĺicado
+let botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+//let corteLinea = document.createElement("br") se comenta por error
+document.body.appendChild(corteLinea)
+document.body.appendChild(botonBorrar);
 
 function agregarInvitado(nombre, edad, nacionalidad) {
 
@@ -44,26 +52,27 @@ function agregarInvitado(nombre, edad, nacionalidad) {
   else if (nacionalidad === "per") {
     nacionalidad = "Peruana"
   }
+}
+let lista = document.getElementById("lista-de-invitados") //Se crea un div en html para agregar este elemento
 
-var lista = document.getElementById("lista-de-invitados") //Se crea un div en html para agregar este elemento
-
-var elementoLista = document.createElement("div")
+let elementoLista = document.createElement("div")
 elementoLista.classList.add("elemento-lista")// Se quita el added, dejando solo add
 lista.appendChild(elementoLista)
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
+let spanNombre = document.createElement("span")
+let inputNombre = document.createElement("input")
+let espacio = document.createElement("br")
 spanNombre.textContent = "Nombre: "
 inputNombre.value = nombre 
 elementoLista.appendChild(spanNombre)
 elementoLista.appendChild(inputNombre)
 elementoLista.appendChild(espacio)
 
+
 function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span");
-var inputNombre = document.createElement("input");
-var espacio = document.createElement("br");
+let spanNombre = document.createElement("span");
+let inputNombre = document.createElement("input");
+let espacio = document.createElement("br");
 spanNombre.textContent = descripcion + ": ";
 inputNombre.value = valor;
 elementoLista.appendChild(spanNombre);
@@ -75,16 +84,15 @@ crearElemento("Nombre", nombre)
 crearElemento("Edad", edad)
 crearElemento("Nacionalidad", nacionalidad)
 
-
-var botonBorrar = document.createElement("button")
+let botonBorrar = document.createElement("button")
 botonBorrar.textContent = "Eliminar invitado"
 botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
+let corteLinea = document.createElement("br")
 elementoLista.appendChild(corteLinea)
 elementoLista.appendChild(botonBorrar);
 
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
+botonBorrar.onclick = function() {
+this.parentNode.style.display = 'none'; //se descomento 
 botonBorrar.parentNode.remove()
+  
   }
-}
